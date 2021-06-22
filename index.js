7a8e5fcd69d9d3579671b55164edd1db723b8490
SukkaW <isukkaw@gmail.com>
2021-06-23 00:31:45 +0800

    Print platform info

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { promises: fsPromises } = require('fs');
 const path = require('path');
 
 (async () => {
+  printPlatformInformation();
+
   const graphModel = await tf.loadGraphModel(`file://${path.resolve(__dirname, 'models/model.json')}`);
   const model = new automl.ImageClassificationModel(graphModel, ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']);
 
@@ -17,3 +19,20 @@ const path = require('path');
     console.log(predictions[0].label, process.memoryUsage().rss, tf.memory().numBytes);
   }
 })();
+
+function printPlatformInformation() {
+  console.log('Platform info:');
+
+  const os = require('os');
+  const { node, v8 } = process.versions;
+  const plat = `OS: ${os.type()} ${os.release()} ${os.arch()}\nNode.js: ${node}\nV8: ${v8}`;
+  const cpus = os.cpus().map(cpu => cpu.model).reduce((o, model) => {
+    o[model] = (o[model] || 0) + 1;
+    return o;
+  }, {});
+  const cpusInfo = Object.keys(cpus).map((key) => {
+    return `${key} x ${cpus[key]}`;
+  }).join('\n');
+
+  console.log(`${plat}\nCPU: ${cpusInfo}\nMemory: ${os.totalmem() / (1024 * 1024)} MiB\n`);
+}
